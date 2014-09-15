3fe309fd09afc6ded0ae642112a1bd27f3fc2436
Michael Anderson <andermic@gmail.com>
2014-09-15 12:42:17 -0600

    content updates

--- schwag/schwag/settings/base.py
+++ schwag/schwag/settings/base.py
@@ -139,6 +139,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.tz',
     'django.contrib.messages.context_processors.messages',
     'django.core.context_processors.request',
+    'senex_shop.core.context_processors.get_default_shop',
 )
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#template-loaders
